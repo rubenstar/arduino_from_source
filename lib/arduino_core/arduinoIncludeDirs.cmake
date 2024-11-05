e626d77536cc69e899b2d55bacc1d8d9e4fbb411
rubenstar <ruben-stadler@hotmail.com>
2024-11-05 21:55:04 +0100

    Move some path defines to arduinoIncludeDirs.cmake

--- lib/arduino_core/arduinoIncludeDirs.cmake
+++ lib/arduino_core/arduinoIncludeDirs.cmake
@@ -1,6 +1,14 @@
 # Interface library for include directories
 # This is packed into a separate file so it can be used by other projects
 
+# ARM SAMD21 family
+set (SAMD_DIR ${ARDUINO_FROM_SOURCE_DIR}/lib/arduino_core/ArduinoCore-samd)
+# ARM CMSIS
+set (ARM_CMSIS_DIR ${ARDUINO_FROM_SOURCE_DIR}/lib/arduino_core/CMSIS_5)
+set (ARDUINO_CMSIS_DIR ${ARDUINO_FROM_SOURCE_DIR}/lib/arduino_core/ArduinoModule-CMSIS-Atmel/CMSIS-Atmel/CMSIS/Device/ATMEL)
+# ARDUINO API
+set (ARDUINO_API_DIR ${ARDUINO_FROM_SOURCE_DIR}/lib/arduino_core/ArduinoCore-API)
+
 add_library(arduinoIncludeDirs INTERFACE)
 target_include_directories(arduinoIncludeDirs INTERFACE
     ${ARDUINO_CMSIS_DIR}/                       # samd.h 
